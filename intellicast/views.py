9d586b4bf4aaa1dd5e6bf327307fcded88c6b08b
bbliss <ben.bliss@gmail.com>
2011-06-14 21:04:13 -0500

    Elegantly handle a weather request for a forecast from the past.  (googlebot)

--- intellicast/views.py
+++ intellicast/views.py
@@ -94,6 +94,13 @@ def weather_page(request):
 def daily_weather_detail(request, year=None, month=None, day=None):
     
     forecast_date = datetime.date(year=int(year), month=int(month), day=int(day))
+    if forecast_date < datetime.date.today():
+        template_name = 'intellicast/daily_weather_detail.html'
+        return render_to_response(template_name, {
+            'unavailable': True
+        }, context_instance = RequestContext(request)) 
+    
+    
     difference = forecast_date - datetime.date.today()
     day_index = str(1 + difference.days)
     
