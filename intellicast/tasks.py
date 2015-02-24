9227fa5b885c59795cd482b810fe2cc71ef645d9
Ben Bliss <ben.bliss@gmail.com>
2015-02-24 11:23:28 -0600

    Added Knoxville, Evansville, Nashville coords for maps.

--- intellicast/tasks.py
+++ intellicast/tasks.py
@@ -73,5 +73,39 @@ def update_map_images():
             duration=0.5,
             subRectangles=False
         )
+
+    #Do it all over with new image for nashville area
+    r = requests.get('http://services.intellicast.com/200904-01/158765827/Image/Radar/Radar2009.13L/Loop/SectorName/r07')
+    
+    #Load up the fetched file from the disk
+    original_file = Image.open(StringIO(r.content))
+    original_file.load()
+    
+    #Set up zipcodes with a list for their image frames
+    frame = 0
+    frames_dict = {}
+    #Loop through the frames of the original images, cropping out frames for each region
+    while True:
+        try:
+            original_file.seek(frame)
+
+            for size, key_dict in settings.INTELLICAST_CROP_DICT_SOUTH.items():
+                for key, crop in key_dict.items():
+                    frame_list = frames_dict.setdefault(key, [])
+                    frame_list.append(original_file.copy().crop(crop).resize(size))
+
+            frame = frame + 1
+        except EOFError:
+            break
+        
+    
+    #Write the new sets of frames to GIF files on the disk.
+    for (zip_code, frame_list) in frames_dict.items():
+        writeGif(
+            filename=settings.MEDIA_ROOT + '/intellicast/intellicast_animated_' + zip_code + '.gif',
+            images=frame_list, 
+            duration=0.5,
+            subRectangles=False
+        )
     
     
